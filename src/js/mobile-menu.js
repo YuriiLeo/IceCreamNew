d80654a7cf0e9842974c7d626e50ff2415e82675
YuriiLeo <yuriileontenko@gmail.com>
2023-05-15 03:02:47 +0200

    add changes for close

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,18 +1,18 @@
 (() => {
-      const refs = {
-        openModalBtn: document.querySelector('[data-mobile-open]'),
-        closeModalBtn: document.querySelector('[data-mobile-close]'),
-        modal: document.querySelector('[data-menu]'),
-        noscroll: document.querySelector("#body1"),
-      };
+  const refs = {
+    openModalBtn: document.querySelector('[data-mobile-open]'),
+    closeModalBtn: document.querySelector('[data-mobile-close]'),
+    modal: document.querySelector('[data-menu]'),
+    noscroll: document.querySelector('#body1'),
+    link: document.querySelector('[link-close]'),
+  };
 
-      refs.openModalBtn.addEventListener('click', toggleModal);
-      refs.closeModalBtn.addEventListener('click', toggleModal);
-   
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.link.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
-    function toggleModal() {
-        refs.openModalBtn.classList.toggle("is-active");
-        refs.modal.classList.toggle('is-open');
-      
-      }
-    })();
+  function toggleModal() {
+    refs.openModalBtn.classList.toggle('is-active');
+    refs.modal.classList.toggle('is-open');
+  }
+})();
